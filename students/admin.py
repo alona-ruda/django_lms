13361e761a363edbafd61cdf89d1d970130315e0
Alona <63323474+alona-ruda@users.noreply.github.com>
2022-11-01 13:24:29 +0100

    lesson 22 admin for students, GroupListFilter, StudentAdmin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
 
-# Register your models here.
+from groups.models import Group
+from .models import Student
+
+class GroupListFilter(admin.SimpleListFilter):
+    title = 'group filter'
+    parameter_name = 'group_filter'
+
+    def lookups(self, request, model_admin):
+        groups = Group.objects.all().order_by('name')
+        lst = [(group.pk, group.name) for group in groups]
+        lst.insert(0, (0, 'No group'))
+        return tuple(lst)
+
+    def queryset(self, request, queryset):
+        match self.value():
+            case None:
+                return Student.objects.all()
+            case '0':
+                return Student.objects.filter(group__isnull=True)
+            case _:
+                return Student.objects.filter(group=Group.objects.get(pk=int(self.value())))
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'group_name')
+    list_display_links = list_display
+    list_per_page = 15
+    # list_filter = ('group__name',)
+    list_filter = (GroupListFilter, )
+
+    def group_name(self, instance):
+        if instance.group:
+            return instance.group.name
+
+        return ''
+
+    group_name.short_description = 'group'
+
+
+admin.site.register(Student, StudentAdmin)
